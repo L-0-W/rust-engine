a1d63a7457720c2ae1ba76454693b50c4b4b737a
l-0-w <typeint@proton.me>
2026-02-02 16:50:13 -0300

    criado vertex/index buffers para criação de geometria mais complexa, utilizando index buffer para diminuir bytes e repetição de vertices

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,37 @@
 mod math;
 
 use std::sync::Arc;
+use wgpu::util::DeviceExt;
 
 use winit::{
     application::ApplicationHandler, dpi::PhysicalPosition, event::*, event_loop::{ActiveEventLoop, EventLoop}, keyboard::{KeyCode, PhysicalKey}, window::Window
 };
 
-enum ShaderInUse {
-    Shader1,
-    Shader2
+enum TrinInUse {
+    Trin1,
+    Trin2
 }
+
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex { position: [-0.0868241, 0.49240386, 0.0], color: [0.5, 0.0, 0.5] }, // A
+    Vertex { position: [-0.49513406, 0.06958647, 0.0], color: [0.5, 0.0, 0.5] }, // B
+    Vertex { position: [-0.21918549, -0.44939706, 0.0], color: [0.5, 0.0, 0.5] }, // C
+    Vertex { position: [0.35966998, -0.3473291, 0.0], color: [0.5, 0.0, 0.5] }, // D
+    Vertex { position: [0.44147372, 0.2347359, 0.0], color: [0.5, 0.0, 0.5] }, // E
+];
+
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
 
 pub struct State {
     surface: wgpu::Surface<'static>,
@@ -18,8 +40,10 @@ pub struct State {
     config: wgpu::SurfaceConfiguration,
     is_surface_configured: bool,
     render_pipeline: wgpu::RenderPipeline,
-    render_pipeline_tri_pos: wgpu::RenderPipeline,
-    shader_in_use: ShaderInUse,
+    triangle_in_use: TrinInUse,
+    vertex_buffer: wgpu::Buffer,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
     window: Arc<Window>,
     clear_color: wgpu::Color
 }
@@ -35,6 +59,19 @@ impl App {
         }
     }
 
+}
+
+impl Vertex {
+
+    const ATTRIBS: [wgpu::VertexAttribute; 2] = wgpu::vertex_attr_array![0 => Float32x3, 1 => Float32x3];
+
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        wgpu::VertexBufferLayout { 
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &Self::ATTRIBS
+        }
+    }
 }
 
 impl ApplicationHandler<State> for App  {
@@ -129,6 +166,7 @@ impl State {
         })
         .await?;
 
+
         let surface_caps = surface.get_capabilities(&adapter);
 
         let surface_format = surface_caps.formats.iter()
@@ -163,7 +201,9 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: Some("vs_main"),
-                buffers: &[],
+                buffers: &[
+                    Vertex::desc(),
+                ],
                 compilation_options: wgpu::PipelineCompilationOptions::default(),
             },
             fragment: Some(wgpu::FragmentState {
@@ -233,7 +273,26 @@ impl State {
             cache: None
         });
 
-        
+
+        let vertex_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Vertex Buffer"),
+                contents: bytemuck::cast_slice(VERTICES),
+                usage: wgpu::BufferUsages::VERTEX,
+            }
+        );
+
+
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+
+    
+        let num_indices = INDICES.len() as u32;
 
         Ok( Self {
             surface,
@@ -242,8 +301,10 @@ impl State {
             config,
             is_surface_configured: false,
             render_pipeline,
-            render_pipeline_tri_pos,
-            shader_in_use: ShaderInUse::Shader1,
+            triangle_in_use: TrinInUse::Trin1,
+            vertex_buffer,
+            index_buffer,
+            num_indices,    
             clear_color: wgpu::Color { r: 0.1, g: 0.3, b: 0.2, a: 1.0 }, 
             window
         })
@@ -266,9 +327,9 @@ impl State {
             (KeyCode::Escape, true) => event_loop.exit(),
             (KeyCode::Space, true) => {
 
-                match  self.shader_in_use {
-                    ShaderInUse::Shader1 => self.shader_in_use = ShaderInUse::Shader2,
-                    ShaderInUse::Shader2 => self.shader_in_use = ShaderInUse::Shader1
+                match  self.triangle_in_use {
+                    TrinInUse::Trin1 => self.triangle_in_use = TrinInUse::Trin2,
+                    TrinInUse::Trin2 => self.triangle_in_use = TrinInUse::Trin1
                 }
 
             }
@@ -321,12 +382,10 @@ impl State {
                     timestamp_writes: None,
             });
 
-            match self.shader_in_use {
-                ShaderInUse::Shader1 => render_pass.set_pipeline(&self.render_pipeline),
-                ShaderInUse::Shader2 => render_pass.set_pipeline(&self.render_pipeline_tri_pos),
-            }
-
-            render_pass.draw(0..3, 0..1);
+            render_pass.set_pipeline(&self.render_pipeline);
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         };
 
         self.queue.submit(std::iter::once(encoder.finish()));
